31f761996ab1c61dffd362c85faf5483eb0268af
Ansh Kakadia <kakadia.an@northeastern.edu>
2026-03-22 20:38:41 -0400

    trying to add colors to polyvore

---
+++ src/data/add_color_to_polyvore.py
@@ -0,0 +1,33 @@
+import requests
+from PIL import Image
+from io import BytesIO
+import json
+from image_dominant_colors import get_most_dominant_colors
+
+
+def get_image_from_url(url: str):
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, timeout=10)
+    response.raise_for_status()
+    return Image.open(BytesIO(response.content))
+
+
+def add_color_to_polyvore(file_path):
+    with open(file_path) as f:
+        data = json.load(f)
+
+    for outfit in data:
+        for clothing_item in outfit["items"]:
+            try:
+                image = get_image_from_url(clothing_item["image"])
+                c1, c2 = get_most_dominant_colors(image, 2)
+                clothing_item["dominant_color"] = c1.tolist()
+                clothing_item["secondary_color"] = c2.tolist()
+            except Exception as e:
+                print(f"Skipping item in outfit {clothing_item["name"]}: {e}")
+
+        with open(file_path, 'w') as f:
+            json.dump(data, f, indent=2)
+        
+if __name__ == "__main__":
+    add_color_to_polyvore("polyvore_dataset/valid_no_dup.json")
--- src/data/image_dominant_colors.py
+++ src/data/image_dominant_colors.py
@@ -3,16 +3,12 @@ from rembg import remove
 import numpy as np
 from sklearn.cluster import KMeans
 
-
-def get_most_dominant_colors(image_id: str, k : int):
-    '''returns the top k dominant colors of the article of clothing using k-means clustering''' 
-    img_path = "./fashion_items/images/" + image_id + ".jpg"
+def get_most_dominant_colors(image: Image.Image, k:int):
+    '''returns the top k dominant colors of the given clothing image'''
     
-    image = Image.open(img_path)
     image_bg_removed = remove(image)
     
     rgba_pixels = np.array(image_bg_removed)
-    print(rgba_pixels)
     filter_transparant = rgba_pixels[:,:,3] > 128
     rgb_pixels = rgba_pixels[filter_transparant][:,:3]
     
@@ -21,11 +17,25 @@ def get_most_dominant_colors(image_id: str, k : int):
     counts = np.bincount(kmeans.labels_)
     sorted_colors = kmeans.cluster_centers_[np.argsort(-counts)]
     return sorted_colors.astype(int)
+
+
+
+def get_most_dominant_colors_kaggle(image_id: str, k : int):
+    '''returns the top k dominant colors of the article of clothing from 
+    
+    the kaggle dataset using k-means clustering''' 
+    
+    img_path = "./fashion_items/images/" + image_id + ".jpg"
+    
+    image = Image.open(img_path)
+    
+    return get_most_dominant_colors(image,k)
+    
     
 
 #Unit test for most_dominant_colors
 def test_dom_colors():
-    result = get_most_dominant_colors("59925", 4)
+    result = get_most_dominant_colors_kaggle("59925", 4)
     for i, color in enumerate(result):
         r, g, b = color
         swatch = f"\033[48;2;{r};{g};{b}m     \033[0m"  # colored block
