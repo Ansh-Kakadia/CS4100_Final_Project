3f8f1b8b621d80acb7b0202e08bc957b933eb42e
Ansh Kakadia <kakadia.an@northeastern.edu>
2026-04-01 14:57:55 -0400

    added cleaned kaggle

--- src/data/clean_invalid_pictures.py
+++ src/data/clean_invalid_pictures.py
@@ -1,6 +1,6 @@
 import json
 
-def clean_data(json_path:str, new_path : str) -> None:
+def clean_data_polyvore(json_path:str, new_path : str) -> None:
     
     with open(json_path) as f:
         data = json.load(f, )
@@ -10,6 +10,15 @@ def clean_data(json_path:str, new_path : str) -> None:
     
     with open(new_path, 'w') as f:
         json.dump(data, f)
+        
+def clean_data_kaggle(json_path: str, new_path):
+    with open(json_path) as f:
+        data = json.load(f)
+        
+    data = [item for item in data if 'dominant_color' in item and 'secondary_color' in item]
+
+    with open(new_path, 'w') as f:
+        json.dump(data, f)
 
 if __name__ == '__main__':
-    clean_data('polyvore_dataset/valid_no_dup.json', 'polyvore_dataset/valid_no_dup_cleaned.json')
+    clean_data_kaggle('fashion_items/styles.json', 'polyvore_dataset/styles_cleaned.json')
